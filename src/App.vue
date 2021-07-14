4868bddab658262f220bc7908176b7349d9986cf
Velerium <bondaniandrea@libero.it>
2021-07-14 17:52:13 +0200

    Added bonus loader!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
+  <div v-if="records.length === 10" id="app">
     <Nav />
     <Main :records="records" />
+  </div>
+  <div v-else class="loading-screen">
+    <Loader />
   </div>
 </template>
 
@@ -9,12 +12,14 @@
 import axios from 'axios'
 import Nav from './components/Nav.vue'
 import Main from './components/Main.vue'
+import Loader from './components/Loader.vue'
 
 export default {
   name: 'App',
   components: {
     Nav,
-    Main
+    Main,
+    Loader
   },
   data () {
     return {
@@ -22,9 +27,11 @@ export default {
     }
   },
   created() {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((reply) => {
-      this.records = reply.data.response;
-    }) 
+    setTimeout(() => { // Gives time to show the loading screen!
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((reply) => {
+        this.records = reply.data.response;
+      }) 
+    }, 2000)
   }
 }
 </script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="loading-screen">
+        <div class="loader">
+            <font-awesome-icon class="loading-icon fa-spin" icon="spinner" />
+            <div class="text">Caricamento in corso!</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Loader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+    @import '../style/fonts';
+    @import '../style/colors';
+
+    .loading-screen {
+        height: 100vh;
+        position: relative;
+        background-color: $main-bg;
+
+        .loader {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 40px;
+            text-align: center;
+            color: $h2-color;
+
+            .text {
+                font-size: 25px;
+                font-family: $main-font;
+                margin-top: 15px;
+            }
+        }
+    }
+</style>
